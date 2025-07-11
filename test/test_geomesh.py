39393c6ea30395bd927d8e699537707a48f788ae
Daniel Ari Friedman <danielarifriedman@gmail.com>
2025-07-11 16:40:52 -0700

    Update

--- test/test_geomesh.py
+++ test/test_geomesh.py
@@ -6,11 +6,7 @@ class TestGeomesh(unittest.TestCase):
     def setUp(self):
         self.geomesh = Geomesh(geo_out_db_dir='test_db')
 
-    def test_get_data(self):
-        result = self.geomesh.get_data('test_dataset', year=2023)
-        self.assertIsInstance(result, list)
-
-    @patch('geo_infer_space.repo.osc_geo_h3grid_srv.geoserver.geomesh.shape.Shape')
+    @patch('osc_geo_h3grid_srv.geoserver.geomesh.shape.Shape')
     def test_filter(self, mock_shape):
         mock_shape_instance = mock_shape.return_value
         mock_shape_instance.get_h3_in_shape.return_value = ['test_cell']
